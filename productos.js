21833c7a07597ba7d29db9b6afd8db48596ee9a8
serbys1 <braulioxp@outlook.com>
2026-06-05 22:37:39 -0500

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,45 @@
+// TU INVENTARIO REAL ACTUALIZADO
+const misProductos = [
+    {
+        id: 1,
+        categoria: "audio",
+        marca: "SAMSUNG",
+        nombre: "Equipo de Sonido Doble",
+        precio: "35.00",
+        // Si tu foto está suelta en la carpeta principal déjala así, si está en una carpeta pon "fotos-audio/20260521_162256.jpg"
+        fotoPrincipal: "20260521_162256.jpg", 
+        fotosGaleria: ["20260521_162256.jpg", "20260521_162417.jpg", "20260521_162731.jpg"],
+        estado: "Usado / Impecable",
+        descripcion: "Sistema de altavoces dobles autoamplificados de gran rendimiento. Cuenta con una excelente respuesta de frecuencias bajas y fidelidad acústica ideal para eventos o uso en salas medianas."
+    },
+    {
+        id: 8,
+        categoria: "muebles",
+        marca: "MUEBLES EL BOSQUE",
+        nombre: "Mueble Organizador de Hogar",
+        precio: "25.00",
+        fotoPrincipal: "fotos-muebles/images-uno.jpg",
+        fotosGaleria: [
+            "fotos-muebles/images-uno.jpg",
+            "fotos-muebles/images-dos.jpg",
+            "fotos-muebles/images-tres.jpg"
+        ],
+        estado: "Usado / Como nuevo",
+        descripcion: "Estantería organizadora ideal para sala o estudio. Estructura firme con múltiples compartimentos."
+    },
+    {
+        id: 9,
+        categoria: "muebles",
+        marca: "MUEBLES EL sala",
+        nombre: "Mueble Organizador",
+        precio: "50.00",
+        fotoPrincipal: "fotos-muebles/images-cuatro.jpg",
+        fotosGaleria: [
+            "fotos-muebles/images-cuatro.jpg",
+            "fotos-muebles/images-sinco.jpg",
+            "fotos-muebles/images-seis.jpg"
+        ],
+        estado: "Usado / Como nuevo",
+        descripcion: "Estantería organizadora ideal para sala o estudio. Estructura firme con múltiples compartimentos."
+    }
+];
